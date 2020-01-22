2403db47b47a3765217ae681976d03e5861ba66f
aakashDeveloper <ahanda205@gmail.com>
2020-01-22 03:10:22 +0000

    a

---
+++ first.js
@@ -0,0 +1,87 @@
+Es5 (Ecam Script)
+
+//////
+DataTypes
+///////
+String = "aaa", 'vv', '10', 'true'  ,"11", "jj' 
+Number= 10, 88.877,  8678678687676.868686
+Boolean= true/false
+
+null
+undefined
+
+001001
+100100
+101101
+
+-------------
+var a = "hiiii"
+var b = 10
+var c = true
+
+
+var a = "hiiii"
+undefined
+typeof(a)
+"string"
+var  b = 10
+undefined
+typeof(b)
+"number"
+var c = true 
+undefined
+typeof(c)
+"boolean"
+
+var a = "hhhhhhh"
+undefined
+var output = typeof(a)
+undefined
+output
+"string"
+var d = "777777.8888"
+undefined
+typeof(d)
+"string"
+var d = 777777.8888
+undefined
+typeof(d)
+"number"
+
+---------------
+
+var a = 10
+var b = 3
+undefined
+a+b
+13
+a-b
+7
+a*b
+30
+a/b
+3.3333333333333335
+a%b
+1
+10%2
+0
+10%3
+1
+10%4
+2
+10%5
+0
+10%6
+4
+2%10
+2
+3%10
+3
+4%10
+4
+5%10
+5
+9%10
+9
+10%10
+0
---
+++ second.js
@@ -0,0 +1,99 @@
+var a = "hi"
+var b = " JavaScript"
+
+var a = "hi"
+var b = " JavaScript"
+undefined
+a+b
+"hi JavaScript"
+a-b
+NaN
+a/b
+NaN
+a*b
+NaN
+a%b
+NaN
+var c = 10
+undefined
+a+c
+"hi10"
+c+a
+"10hi"
+c+c
+20
+a+a
+"hihi"
+a-c
+NaN
+
+
+////
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+/////
+1+'1'+'1' = '111'
+'1'+1+1   = '111'
+1+'1'+1   = '111'
+1+1+'1'   = '21'
+////
+111 
+12 
+111
+///
+
+1+1+'1'-1
+
+1+'1'+'1'
+"111"
+'1'+1+1
+"111"
+1+'1'+1
+"111"
+1+1+'1'
+"21"
+1+1+'1'-1
+20
+'111'-1
+110
+'21'-1
+20
+'a1'-1
+NaN
+'21'-1
+20
+'21'*2
+42
+'a1'-1
+NaN
+'111111'-10
+111101
+'113333251'-10
+113333241
+
+
+true == 0
+false == 1
+
+true+true
+2
+true+false
+1
+false+false
+0
+2-true
+1
+"3a"
+2+true+'1'-2
+29
+2+true+'1'
+
+"31"-2
+29
+
+2+true+'1'-22
+9
+2+true+'1'-2
+29
