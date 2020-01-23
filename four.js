4b97f09f7f3f3bac255698b9b77a7484b356ebe1
aakashDeveloper <ahanda205@gmail.com>
2020-01-23 03:13:05 +0000

    a

---
+++ four.js
@@ -0,0 +1,60 @@
+console.log('hiii') //debug 
+
+
+****singleline comment***
+//hiii
+
+****multiline comment***
+/*
+dskkvng
+dnnkjf
+lksdfk
+snkfnk
+*/
+
+//popups
+alert('Form submiited')
+undefined
+alert('hiii')
+
+
+//user confirmation
+confirm('Do you want to leave')
+confirm('Do you want to leave')
+true
+confirm('Do you want to leave')
+false
+confirm('Do you want to leave')
+false
+var out =confirm('Do you want to leave')
+undefined
+out
+true
+var out =confirm('Do you want to leave')
+undefined
+out
+false
+
+
+// user input
+prompt('What is your age')
+"33333"
+prompt('What is your age')
+null
+var out = prompt('What is your age')
+undefined
+out
+"2342"
+var out = prompt('What is your age')
+undefined
+out
+null
+
+//assignment
+var a = prompt('Enter first value?');
+var b = prompt('Enter second value?')
+console.log(a+b)
+a = '1'
+b = '1'
+a+b = '11'
+a+b = 2
---
+++ third.js
@@ -0,0 +1,63 @@
+= assigment operator
+== comapring the value
+=== comapring data and datatype
+
+
+var a = 10
+a == 10
+a === '10'
+
+var a = 10
+undefined
+a
+10
+a == 10
+true
+a == '10'
+true
+a === '10'
+false
+a === 10
+true
+var b = 'true'
+undefined
+b == true
+false
+b === 'true'
+true
+
+var a = '1'
+undefined
+typeof(a)
+"string"
+1 == a
+true
+1 === a
+false
+var b = 'true'
+undefined
+typeof(b)
+"string"
+true == b
+false
+true === b
+false
+'true' === b
+true
+
+----------------------------
+Es5   ======>  var
+----------------------------
+Es6   ======>  let , const
+
+
+--------------
+var a = 10
+
+var a = "declaration"
+a = 10 "assignment"
+
+
+var => redecalre and reassign
+let = we cannot redecalre but can reassign
+const = you cannot redecalre nor reassign
