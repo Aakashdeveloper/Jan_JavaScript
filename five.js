ca0a1fcca6f06a905813eeff05f87ce057bc051c
aakashDeveloper <ahanda205@gmail.com>
2020-01-24 03:16:59 +0000

    a

---
+++ five.js
@@ -0,0 +1,70 @@
+var city= "london"
+undefined
+city
+"london"
+city.toUpperCase()
+"LONDON"
+
+var city1= "NEWYORK"
+undefined
+city1.toLowerCase()
+"newyork"
+
+var a = "john is my name"
+undefined
+a.length
+15
+var a = "   john is my name    "
+undefined
+a.trim()
+"john is my name"
+var a = "   john is my name    "
+undefined
+a.length
+22
+var b = a.trim()
+undefined
+b.length
+15
+var a = "   john is my name    "
+undefined
+a.trim().length
+15
+
+var city = "London"
+undefined
+city[3]
+"d"
+city[0]
+"L"
+city[6]
+undefined
+city.length
+6
+city[city.length-1]
+"n"
+
+var a = "london"
+undefined
+a.slice(1)
+"ondon"
+a.slice(2)
+"ndon"
+a.slice(2,4)
+"nd"
+
+var name="JOhN"
+var name ="jMmaY"
+
+var name ="jMmaY"
+undefined
+name[0]
+"j"
+name[0].toUpperCase()
+"J"
+name.slice(1)
+"MmaY"
+name.slice(1).toLowerCase()
+"mmay"
+name[0].toUpperCase()+name.slice(1).toLowerCase()
+"Jmmay"
--- four.js
+++ four.js
@@ -51,10 +51,18 @@ out
 null
 
 //assignment
+var a = parseInt(prompt('Enter first value?'));
 var a = prompt('Enter first value?');
 var b = prompt('Enter second value?')
-console.log(a+b)
+console.log(parseInt(a+b))
+console.log(parseInt(a)+parseInt(b))
+
 a = '1'
 b = '1'
 a+b = '11'
 a+b = 2
+
+
+var a = parseInt('1')
+
+parseFloat(a)+parseFloat(b)
