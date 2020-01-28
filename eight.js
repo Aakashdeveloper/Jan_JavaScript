1fa79f60d8d00beb5e794d020d85631415238a4d
aakashDeveloper <ahanda205@gmail.com>
2020-01-28 03:22:43 +0000

    a

---
+++ eight.js
@@ -0,0 +1,87 @@
+
+
+if(logic){
+    if logic pass
+}else{
+    if logic fail
+}
+
+if(logic){}
+else if(){}
+else{}
+
+if(logic){}
+if(){}
+if(){}
+
+var a = 10
+if(a%2==0){
+    console.log("Number is even")
+}else{
+    console.log("Number is odd")
+}
+
+
+if(a%2==0){
+    console.log("Number is even")
+}else if(a%3==0 || a%5==0){
+    console.log("Number is divisible by 3")
+}
+else{
+    console.log("Number is odd")
+}
+var b = 10
+var a = b==10
+if(a){
+    console.log('hii')
+}else{
+    console.log('bie')
+}
+
+
+var a = [1,2,3,4]
+if(a){
+    console.log('hii')
+}else{
+    console.log('bie')
+}
+
+
+var c= '88'
+if(c){
+    console.log('hii')
+}else{
+    console.log('bie')
+}
+
+
+//Ternery
+var a = 10
+
+a>10?'first':'Second'
+
+var a = 10
+
+a>10?'first':'Second'
+"Second"
+var a = 10
+
+a==10?'first':'Second'
+"first"
+
+var a = 10
+
+a==10?a+1:a-1
+11
+var a = 10
+
+a>10?a+1:a-1
+9
+
+
+/////
+Luck Game
+
+> Generate one number between 1 to 100
+> take one input as number from user
+> if both match say user win  else user loose 
---
+++ seven.js
@@ -0,0 +1,43 @@
+var a = 10
+a>10
+a<10
+a>=10
+a<=10
+var a = 10
+a>10
+
+false
+var a = 10
+a<10
+
+false
+var a = 10
+a<=10
+
+true
+var a = 10
+a>=10
+
+true
+a !== 10
+false
+a != 10
+false
+
+var a = true
+
+var a = true
+undefined
+!a
+false
+var b = !a
+undefined
+b
+false
+!b
+true
+
+true && true  = true
+false && true  = false
+true && false  = false
+false && false  = false
